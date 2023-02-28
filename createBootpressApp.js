9e866f06b86895812e711ea309127b02c8248918
ufukbakan <println.ufukbakan@gmail.com>
2023-02-28 10:09:12 +0300

    initial upload

---
+++ createBootpressApp.js
@@ -0,0 +1,7 @@
+function init(){
+    console.log(process.env.npm_config_user_agent);
+}
+
+module.exports = {
+    init
+}
---
+++ index.js
@@ -0,0 +1,13 @@
+console.log("hello world");
+// const process = require("process");
+
+// const majorNodeVersion = process.versions.node.split('.')[0];
+// console.log("Node version: " + majorNodeVersion);
+// if (majorNodeVersion < 10) {
+//     console.error('Express.js requires node version 10.X.X+ ');
+//     process.exit(1);
+// }
+
+// const { init } = require('./createBootpressApp');
+
+// init();
